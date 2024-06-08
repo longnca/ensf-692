ea3ff27a66096f7159f56e0e1ece38834a79d52e
longnca <dsbuff01@gmail.com>
2024-06-08 16:27:41 -0600

    refactor codes by splitting them into modular functions for easier control

--- Assignment_4/calgary_dogs.py
+++ Assignment_4/calgary_dogs.py
@@ -15,8 +15,8 @@ def main():
     # Import data here
     df = pd.read_excel("CalgaryDogBreeds.xlsx")
 
-    # Check the first 5 rows of DF
-    print(df.head())
+    # # Check the first 5 rows of DF
+    # print(df.head())
 
     # # My personal check: a quick EDA of the dataset
     # print(df.info())              # Check the info of the dataframe
@@ -31,56 +31,68 @@ def main():
     df.set_index(['Breed', 'Year', 'Month'], inplace=True)
     df.sort_index(inplace=True)
 
-    # Check the DataFrame after setting the multi-index
-    print(df.head(10))
+    # # Check the DataFrame after setting the multi-index if needed
+    # print(df.head())
 
     print("\nENSF 692 Dogs of Calgary")
 
     # Stage 2: User input stage
+    user_input = get_user_input(df)
+
+    # Stage 3: Data analysis stage
+    calculate_breed_stats(df, user_input)
+
+
+def get_user_input(df):
+    """
+
+    :param df:
+    :return:
+    """
     while True:
         try:
             # Take the user input and convert it to uppercase
             user_input = input("Please enter a dog breed: ").upper()
             # Check if the breed exists in the index
             if user_input in df.index.get_level_values('Breed'):
-                # Extract the specific breed name
-                breed_data = df.loc[pd.IndexSlice[user_input, :, :]]
-
-                # Find all the years from the 'year' index of the breed input
-                breed_years = ", ".join(map(str, breed_data.index.get_level_values('Year').unique()))
-                # Find and print all years if he selected breed was found
-                print(f"The {user_input} was found in the top breeds for years: {breed_years}. ")
-
-                # Calculate and print the total number of registrations of the selected breed found
-                total_breed_found = breed_data['Total'].sum()
-                print(f"There have been {total_breed_found} {user_input} dogs registered total.")
-
-                # Calculate and print the percentage of selected breed registrations
-                # out of the total percentage for each year
-                total_dogs_2021 = df.loc[pd.IndexSlice[:, 2021, :], 'Total'].sum()
-                total_dogs_2022 = df.loc[pd.IndexSlice[:, 2022, :], 'Total'].sum()
-                total_dogs_2023 = df.loc[pd.IndexSlice[:, 2023, :], 'Total'].sum()
-
-                selected_breed_2021 = breed_data.loc[pd.IndexSlice[:, 2021, :], 'Total'].sum()
-                selected_breed_2022 = breed_data.loc[pd.IndexSlice[:, 2022, :], 'Total'].sum()
-                selected_breed_2023 = breed_data.loc[pd.IndexSlice[:, 2023, :], 'Total'].sum()
-
-                percentage_breed_2021 = (selected_breed_2021 / total_dogs_2021) * 100
-                percentage_breed_2022 = (selected_breed_2022 / total_dogs_2022) * 100
-                percentage_breed_2023 = (selected_breed_2023 / total_dogs_2023) * 100
-
-                print(f"The {user_input} was {percentage_breed_2021}% of top breeds in 2021.")
-                print(f"The {user_input} was {percentage_breed_2022}% of top breeds in 2022.")
-                print(f"The {user_input} was {percentage_breed_2023}% of top breeds in 2023.")
-                # After successful data entry and analysis, the program ends
-                break
+                return user_input
             else:
                 # If the dog breed is not found, raise a KeyError exception
                 raise KeyError
         except KeyError as e:
             print("Dog breed not found in the data. Please try again.")
 
-    # Stage 3: Data analysis stage
+
+def calculate_breed_stats(df, breed):
+    """
+
+    :param df:
+    :param breed:
+    """
+    # Extract the specific breed name
+    breed_data = df.loc[pd.IndexSlice[breed, :, :]]
+
+    # Find all the years from the 'year' index of the breed input
+    breed_years = ", ".join(map(str, breed_data.index.get_level_values('Year').unique()))
+    # Find and print all years if he selected breed was found
+    print(f"The {breed} was found in the top breeds for years: {breed_years}. ")
+
+    # Calculate and print the total number of registrations of the selected breed found
+    total_breed_found = breed_data['Total'].sum()
+    print(f"There have been {total_breed_found} {breed} dogs registered total.")
+
+    # Calculate and print the percentage of selected breed registrations for each year
+    # Create a list of the years from the original DF in the Excel file
+    years = [2021, 2022, 2023]
+    # Iterate over the list of years to calculate the total/percentage of the selected breed
+    for year in years:
+        try:
+            total_dogs_per_year = df.loc[pd.IndexSlice[:, year, :], 'Total'].sum()
+            selected_breed_per_year = breed_data.loc[pd.IndexSlice[:, year, :], 'Total'].sum()
+            percentage_breed_per_year = (selected_breed_per_year / total_dogs_per_year) * 100
+            print(f"The {breed} was {percentage_breed_per_year:.5f}% of top breeds in {year}.")
+        except KeyError as e:
+            print(f"Error accessing data for the year {year}: {e}")
 
 
 if __name__ == '__main__':
